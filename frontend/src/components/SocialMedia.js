42a608dfe844ae53589c5c888ecfdd9a3ffba4a3
scoxen1 <sean.coxen@gmail.com>
2022-02-10 13:07:00 -0500

    minor fixes from npm install, remove package-lock.json from source control - only maintain locally

--- frontend/src/components/SocialMedia.js
+++ frontend/src/components/SocialMedia.js
@@ -5,15 +5,6 @@ import linkedin from './img/linkedin.svg'
 import pinterest from './img/pinterest.svg'
 import twitter from './img/twitter.svg'
 import youtube from './img/youtube.svg'
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-//import {
-//    faYoutube,
-//    faFacebook,
-//    faTwitter,
-//    faInstagram,
-//    faPinterest,
-//    faLinkedin
-//} from "@fortawesome/free-brands-svg-icons";
 import './styles/SocialMedia.scss';
 
 export default function SocialMedia(props) {
@@ -21,40 +12,6 @@ export default function SocialMedia(props) {
     //-------------------------------------------------------------------
     // Region: Render helpers
     //-------------------------------------------------------------------
-    /*
-    const renderIcon = (iconName) => {
-        switch (iconName) {
-            case "faFacebook":
-                return faFacebook;
-            case "faInstagram":
-                return faInstagram;
-            case "faTwitter":
-                return faTwitter;
-            case "faYoutube":
-                return faYoutube;
-            case "faPinterest":
-                return faPinterest;
-            case "faLinkedIn":
-                return faLinkedin;
-            default:
-                return null;
-        }
-    }
-
-    const renderItems = () => {
-        return (
-            props.items.map((item, counter) => {
-                var icon = renderIcon(item.icon);
-                return (
-                    <a href={item.url} key={item.icon} target="_blank"
-                        className={`${item.css} social m-0 ${counter === 0 ? '' : 'ml-2 ml-md-3'}`} >
-                        <FontAwesomeIcon icon={icon} size="2x" title={item.url} />
-                    </a>
-                )
-            })
-        )
-    }
-    */
     const renderIcon = (iconName) => {
         switch (iconName.toLowerCase()) {
             case "fafacebook":
