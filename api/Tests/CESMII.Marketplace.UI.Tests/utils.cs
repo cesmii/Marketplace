30038977f469b133868c7cdd499457df15288425
Paul Yao <paul@paulyao.com>
2023-06-20 18:16:34 -0400

    New test - Selenium using web driver with chrome driver

---
+++ api/Tests/CESMII.Marketplace.UI.Tests/utils.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CESMII.Marketplace.UI.Tests
+{
+    internal class utils
+    {
+        public static void PublisherListShowAll(IWebDriver driver)
+        {
+            try
+            {
+                var eleMoreLess = driver.FindElement(By.CssSelector(".info-section:nth-child(3) > .btn"));
+                if (eleMoreLess != null)
+                {
+                    string strValue = eleMoreLess.Text;
+                    if (strValue == "+ See all")
+                        eleMoreLess.Click();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
