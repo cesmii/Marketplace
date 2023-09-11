353af297238ba3ec65e862a6805680970f131c0a
Paul Yao <paul@paulyao.com>
2023-09-11 15:33:07 -0700

    In Browser Unit Test - Accessing log information from a docker container

---
+++ CESMII.LogClient/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CESMII.LogClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CESMII_LogClient: **********************************");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 1");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 2");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 3");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 4");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 5");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 6");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 7");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 8");
+            Console.WriteLine("CESMII_LogClient: Does this go anywhere that I can see? 9");
+            Console.WriteLine("CESMII_LogClient: **********************************");
+        }
+    }
+}
