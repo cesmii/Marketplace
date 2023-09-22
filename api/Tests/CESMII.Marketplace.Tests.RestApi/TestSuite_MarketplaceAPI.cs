bf976da1c6017ea4ec9776f92b74aa4ddbae4035
Paul Lin Yao <paul@paulyao.com>
2023-09-22 11:11:33 -0400

    In Browser Unit Test - Tests RestApi ports

--- api/Tests/CESMII.Marketplace.Tests.RestApi/TestSuite_MarketplaceAPI.cs
+++ api/Tests/CESMII.Marketplace.Tests.RestApi/TestSuite_MarketplaceAPI.cs
@@ -8,6 +8,86 @@ namespace CESMII.Marketplace.RestApi
     public class TestSuite_MarketplaceAPI
     {
         [Fact]
+        public void MarketItemsAvailable_On_Attempt_001()
+        {
+            string strHostHttps = "http://localhost/api";
+            HttpClient client = new HttpClient();
+
+            Console.WriteLine($"MarketItemsAvailable_On_Attempt_001: Attempting to connect to {strHostHttps}");
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt_001: Attempting to connect to {strHostHttps}");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var items = GetFirstItem(client, $"{strHostHttps}/Marketplace/All");
+            string strReturned = (items==null) ? "null" : $"Success! count = items.Count()";
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt_001: Attempting to connect to {strHostHttps}");
+            //Assert.NotNull(items);
+
+            //int count = items.Count();
+            //Assert.NotEqual(0, count);
+        }
+
+        [Fact]
+        public void MarketItemsAvailable_On_Attempt_002()
+        {
+            string strHostHttps = "https://localhost/api";
+            HttpClient client = new HttpClient();
+
+            Console.WriteLine($"MarketItemsAvailable_On_Attempt_002: Attempting to connect to {strHostHttps}");
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt:002: Attempting to connect to {strHostHttps}");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var items = GetFirstItem(client, $"{strHostHttps}/Marketplace/All");
+            string strReturned = (items == null) ? "null" : $"Success! count = items.Count()";
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt_002: Attempting to connect to {strHostHttps}");
+            //Assert.NotNull(items);
+
+            //int count = items.Count();
+            //Assert.NotEqual(0, count);
+        }
+
+        [Fact]
+        public void MarketItemsAvailable_On_Attempt_003()
+        {
+            string strHostHttps = "http://localhost:5000/api";
+            HttpClient client = new HttpClient();
+
+            Console.WriteLine($"MarketItemsAvailable_On_Attempt_003: Attempting to connect to {strHostHttps}");
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt:003: Attempting to connect to {strHostHttps}");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var items = GetFirstItem(client, $"{strHostHttps}/Marketplace/All");
+            string strReturned = (items == null) ? "null" : $"Success! count = items.Count()";
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt_003: Attempting to connect to {strHostHttps}");
+            //Assert.NotNull(items);
+
+            //int count = items.Count();
+            //Assert.NotEqual(0, count);
+        }
+
+        [Fact]
+        public void MarketItemsAvailable_On_Attempt_004()
+        {
+            string strHostHttps = "http://localhost:5000/api";
+            HttpClient client = new HttpClient();
+
+            Console.WriteLine($"MarketItemsAvailable_On_Attempt_004: Attempting to connect to {strHostHttps}");
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt:004: Attempting to connect to {strHostHttps}");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var items = GetFirstItem(client, $"{strHostHttps}/Marketplace/All");
+            string strReturned = (items == null) ? "null" : $"Success! count = items.Count()";
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_Attempt_004: Attempting to connect to {strHostHttps}");
+            //Assert.NotNull(items);
+
+            //int count = items.Count();
+            //Assert.NotEqual(0, count);
+        }
+
+        [Fact]
         public void MarketItemsAvailable_On_RestCall1()
         {
             string strHostHttps = "http://localhost:5000/api";
@@ -18,6 +98,7 @@ namespace CESMII.Marketplace.RestApi
                 strHostHttps = strTemp;
 
             Console.WriteLine($"MarketItemsAvailable_On_RestCall1: Attempting to connect to {strHostHttps}");
+            Console.WriteLine($"::notice::MarketItemsAvailable_On_RestCall1: Attempting to connect to {strHostHttps}");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
@@ -65,7 +146,7 @@ namespace CESMII.Marketplace.RestApi
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Exception: {ex.Message}");
+                Console.WriteLine($"::error::Exception in GetFirstItem: {ex.Message}");
             }
 
             return ReturnValue;
