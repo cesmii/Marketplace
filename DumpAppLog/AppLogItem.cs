29307b3883216948d9f6d2332fd327fa4c5f01d9
Paul Lin Yao <paul@paulyao.com>
2023-09-27 18:43:30 -0400

    In Browser Unit Test

---
+++ DumpAppLog/AppLogItem.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumpAppLog
+{
+    [BsonIgnoreExtraElements]
+
+    public class AppLogItem : AbstractEntity
+    {
+        public DateTime Date { get; set; }
+        public string Level { get; set; }
+        public string Message { get; set; }
+        public string Logger { get; set; }
+        public int ThreadID{ get; set; }
+        public string ThreadName { get; set; }
+        public int ProcessID { get; set; }
+        public string ProcessName { get; set; }
+        public object Properties { get; set; }
+    }
+
+    public abstract class AbstractEntity
+    {
+        //TBD - revisit making ID a string. 
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string ID { get; set; }
+
+    }
+}
---
+++ DumpAppLog/Program.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Text.Json;
+using System;
+
+namespace DumpAppLog
+{
+    internal class Program
+    {
+        private static string? strConnection = "mongodb://testuser:password@localhost:27017";
+
+        private static string? strDatabase = "test";
+
+        static void Main(string[] args)
+        {
+            strConnection = Environment.GetEnvironmentVariable("MARKETPLACE_MONGODB_CONNECTIONSTRING");
+            strDatabase = Environment.GetEnvironmentVariable("MARKETPLACE_MONGODB_DATABASE");
+
+            Console.WriteLine("Dump App Log");
+
+            MongoClient client = new MongoClient(strConnection);
+
+            var db = client.GetDatabase(strDatabase);
+
+            var lookupItems = db.GetCollection<AppLogItem>("app_log");
+
+            var docAppLogItems = lookupItems.Find(new BsonDocument()).ToList();
+
+            for (int i = 0; i < docAppLogItems.Count; i++)
+            {
+                DateTime d = docAppLogItems[i].Date;
+                string strLevel = docAppLogItems[i].Level;
+                string strMessage = docAppLogItems[i].Message;
+                string strLogger = docAppLogItems[i].Logger;
+                string strDate = $"{d.Year:0000}-{d.Month:00}-{d.Day:00} {d.Hour:00}:{d.Minute:00}:{d.Second:00}";
+
+                Console.WriteLine($"{strDate}\t{strLevel}\t{strLogger}\t{strMessage}");
+            }
+        }
+    }
+}
